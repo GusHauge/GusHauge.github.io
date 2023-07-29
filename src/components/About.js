6d4fc45d2ab7ebfdf16c9cab3ee1113d34b6c18c
Augustine Hauge <augustinehauge@gmail.com>
2023-07-29 12:55:28 -0500

    Added experience

--- src/components/About.js
+++ src/components/About.js
@@ -2,16 +2,16 @@ export const About = () => {
   return (
     <div
       id="about"
-      class="flex flex-col w-full h-fit bg-slate-900 items-center space-y-20 py-28 px-10"
+      class="flex flex-col w-full h-fit bg-slate-900 items-center space-y-28 py-28 px-10"
     >
       <h1 class="text-6xl text-white">About Me</h1>
       <div class="flex items-center justify-center space-x-16">
         <img
           src={require("../images/Bray_Frontal.jpg")}
           alt="Gus"
-          class="rounded-lg h-80"
+          class="rounded-lg h-96"
         />
-        <p class="text-lg text-white max-w-2xl w-1/2">
+        <p class="text-lg text-white max-w-lg">
           I am a senior at the University of Notre Dame pursuing a Bachelor of
           Arts in Computer Science and Theology, a unique combination that
           reflects my enthusiasm for both technology and pursuit of the common
@@ -19,8 +19,8 @@ export const About = () => {
           elite universities for low-income students, I have learned the
           importance of hard work and grit. I am extremely interested in machine
           learning, computer vision, tech ethics, data science, and web
-          development. I love learning new skills to solve diverse problems as I
-          kickstart my career.
+          development. I am always looking to learn new skills and solve diverse
+          problems as I kickstart my career.
         </p>
       </div>
     </div>
--- src/components/Experience.js
+++ src/components/Experience.js
@@ -2,22 +2,85 @@ export const Experience = () => {
   return (
     <div
       id="experience"
-      class="flex flex-col w-full h-fit bg-indigo-950 items-center space-y-20 py-28 px-10"
+      class="flex flex-col w-full h-fit bg-indigo-950 items-center space-y-28 py-28 px-10"
     >
       <h1 class="text-6xl text-white">Experience</h1>
-      <div class="flex flex-col gap-14">
-        <div class="flex items-center justify-center space-x-16">
+      <div class="flex flex-col gap-24">
+        <div class="flex  justify-center space-x-16">
           <img
             src={require("../images/IBM_logo.png")}
             alt="IBM"
-            class="rounded h-40"
+            class="rounded-2xl h-52"
           />
-          <p class="text-lg text-white max-w-2xl w-1/2">
-            As a software engineer intern, I have been building a full stack web
-            application using React.js, Express.js, and Postgres to streamline
-            the IBM Enterprise Federation onboarding experience and data access
-            for over 900 business partners.
-          </p>
+          <div class="flex flex-col space-y-4 max-w-xl">
+            <div class="flex justify-between">
+              <h1 class="text-lg font-bold text-white">IBM</h1>
+              <h1 class="text-lg font-bold text-white">
+                Software Engineer Intern
+              </h1>
+            </div>
+            <p class="text-md text-white">
+              This summer I have been building a full stack web application
+              using React.js, Express.js, and Postgres to streamline the IBM
+              Enterprise Federation onboarding experience and data access for
+              over 900 business partners. I have honed my skills in agile
+              methodologies, full stack web development, and database management
+              while improving my proficiency in JavaScript and CSS.
+            </p>
+          </div>
+        </div>
+
+        <div class="flex  justify-center space-x-16">
+          <div class="flex flex-col space-y-4 max-w-xl">
+            <div class="flex justify-between">
+              <h1 class="text-lg font-bold text-white">
+                Computer Vision Research Lab
+              </h1>
+              <h1 class="text-lg font-bold text-white">Research Assistant</h1>
+            </div>
+            <p class="text-md text-white">
+              For the past 2 years I have been working under Professors Adam
+              Czajka and Patrick Flynn at the University of Notre Dame. I have
+              taken on a number of tasks, most notably a NIST iris project in
+              which I used Python scripting and Pandas to filter 500,000 human
+              iris images to a dataset of 6,000 images specified by quality
+              metric software. I have also tested project code to build Python
+              environment files and craft professional cover pages.
+            </p>
+          </div>
+          <img
+            src={require("../images/ND_Research_logo.jpg")}
+            alt="IBM"
+            class="rounded-2xl h-52"
+          />
+        </div>
+
+        <div class="flex  justify-center space-x-10">
+          <img
+            src={require("../images/ND_OIT_logo.png")}
+            alt="IBM"
+            class="rounded-2xl h-52"
+          />
+          <div class="flex flex-col space-y-4 max-w-xl">
+            <div class="flex justify-between">
+              <h1 class="text-lg font-bold text-white">
+                Office of Information Technologies
+              </h1>
+              <h1 class="text-lg font-bold text-white">
+                Classroom Triage Assistant
+              </h1>
+            </div>
+            <p class="text-md text-white">
+              During fall of 2021 I took my first ever tech-related job with the
+              University of Notre Dame's IT office. I primarily managed calls
+              and made classroom visits to aid professors and teaching
+              assistants to help with a variety of issues related to classroom
+              technology in a time when hybrid learning was in effect. I learned
+              how to operate in high-pressure situations where individuals
+              needed immediate help with technology, communicating with my team
+              to provide optimal service.
+            </p>
+          </div>
         </div>
       </div>
     </div>
