25ba62043c2ec2094a8b0698150a02091041202e
Augustine Hauge <augustinehauge@gmail.com>
2023-07-29 16:18:36 -0500

    Changed title height

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,7 +1,7 @@
 export const Title = () => {
   return (
     <div class="flex flex-col w-full h-screen bg-gradient-to-r from-black to-indigo-900 space-y-10 items-center">
-      <h1 class="text-6xl text-white mt-40">Gus Hauge</h1>
+      <h1 class="text-6xl text-white mt-[30vh]">Gus Hauge</h1>
       <h2 class="text-lg text-white">
         Software Developer with a passion for solving problems
       </h2>
