8c1a3771702256f6a45c179ded341e4663efb814
Augustine Hauge <augustinehauge@gmail.com>
2023-07-29 19:42:57 -0500

    Added March Metrics to Projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -5,6 +5,53 @@ export const Projects = () => {
       class="flex flex-col w-full h-fit bg-slate-900 items-center space-y-28 py-28 px-10"
     >
       <h1 class="text-6xl font-thin">Projects</h1>
+      <div class="flex flex-col gap-24">
+        <div class="flex justify-center space-x-10">
+          <a
+            href="https://colab.research.google.com/drive/1j5wOZnaysJeZHw4W5ds9ZSGSeTcwLlGR"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img
+              src={require("../images/March_Metrics_code.png")}
+              alt="March Metrics code"
+              class="h-72 rounded-2xl"
+            />
+          </a>
+          <div class="flex flex-col space-y-4 max-w-xl">
+            <div class="flex justify-between">
+              <h1 class="text-lg font-bold">
+                March Metrics Tournament Predictor
+              </h1>
+              <a
+                href="https://github.com/GusHauge/Silvestri_Carpets"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <img
+                  src={require("../images/GitHub_logo.jpg")}
+                  alt="GitHub"
+                  class="h-8 rounded-full"
+                />
+              </a>
+            </div>
+            <p>
+              In this final project for my Data Structures class, my team and I
+              trained a random forest classifier in Python using the
+              Scikit-Learn library and college basketball data from 2013-2019.
+              Our model had a multiple events that it predicted, ranging from
+              head-to-head matchups to post-season seeding and even March
+              Madness tournament performance for NCAA teams. We incorporated
+              Pandas features like dummy variables and feature importance to
+              refine our model and identify the most significant statistics for
+              post-season success. We delivered weekly memos and code reviews
+              for our class, culminating in a final presentation and code
+              demonstration that earned us an honorable mention for best project
+              out of a pool of 135 students.
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
