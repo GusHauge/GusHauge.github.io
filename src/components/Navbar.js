879e60edeb9d76e67448f0efda1511dd7fb8e7ed
Augustine Hauge <augustinehauge@gmail.com>
2023-08-17 16:54:43 -0400

    Menu fade effect

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,18 @@ import { Bars3Icon } from "@heroicons/react/24/solid";
 import { useState } from "react";
 
 export const Navbar = () => {
-  const [expanded, setExpanded] = useState(false);
+  const [menuState, setMenuState] = useState("hidden");
+  const [fade, setFade] = useState(false);
+
+  const handleMenu = () => {
+    if (fade) {
+      setTimeout(() => setMenuState("hidden"), 300);
+      setFade(false);
+    } else {
+      setMenuState("flex-col");
+      setFade(true);
+    }
+  };
 
   return (
     <div className="flex justify-between bg-indigo-900 items-center h-12 w-full">
@@ -16,55 +27,57 @@ export const Navbar = () => {
 
       <div className="flex flex-col items-center relative sm:hidden">
         <button
-          onClick={() => setExpanded(!expanded)}
+          onClick={() => {
+            handleMenu();
+          }}
           className="transition-colors rounded-md duration-300 delay-100 hover:bg-indigo-500"
         >
           <Bars3Icon className="h-11" />
         </button>
-        {expanded && (
-          <>
-            <div className="flex flex-col absolute mt-11 items-center justify-center my-auto bg-indigo-900">
-              <button
-                className="h-10 w-28 transition-colors duration-300 delay-100 hover:bg-indigo-500"
-                onClick={() => window.scrollTo(0, 0)}
-              >
-                Home
-              </button>
-              <button
-                className="h-10 w-28 transition-colors duration-300 delay-100 hover:bg-indigo-500"
-                onClick={() =>
-                  document.getElementById("about").scrollIntoView()
-                }
-              >
-                About
-              </button>
-              <button
-                className="h-10 w-28 transition-colors duration-300 delay-100 hover:bg-indigo-500"
-                onClick={() =>
-                  document.getElementById("experience").scrollIntoView()
-                }
-              >
-                Experience
-              </button>
-              <button
-                className="h-10 w-28 transition-colors duration-300 delay-100 hover:bg-indigo-500"
-                onClick={() =>
-                  document.getElementById("projects").scrollIntoView()
-                }
-              >
-                Projects
-              </button>
-              <button
-                className="h-10 w-28 transition-colors duration-300 delay-100 hover:bg-indigo-500"
-                onClick={() =>
-                  document.getElementById("contact").scrollIntoView()
-                }
-              >
-                Contact
-              </button>
-            </div>
-          </>
-        )}
+        <div
+          className={`absolute mt-11 items-center justify-center my-auto transition-all duration-300 ${
+            fade ? "opacity-100" : "opacity-0"
+          }`}
+        >
+          <div className={`${menuState}  bg-indigo-900`}>
+            <button
+              className="h-10 w-28 transition-colors duration-300 delay-100 hover:bg-indigo-500"
+              onClick={() => window.scrollTo(0, 0)}
+            >
+              Home
+            </button>
+            <button
+              className="h-10 w-28 transition-colors duration-300 delay-100 hover:bg-indigo-500"
+              onClick={() => document.getElementById("about").scrollIntoView()}
+            >
+              About
+            </button>
+            <button
+              className="h-10 w-28 transition-colors duration-300 delay-100 hover:bg-indigo-500"
+              onClick={() =>
+                document.getElementById("experience").scrollIntoView()
+              }
+            >
+              Experience
+            </button>
+            <button
+              className="h-10 w-28 transition-colors duration-300 delay-100 hover:bg-indigo-500"
+              onClick={() =>
+                document.getElementById("projects").scrollIntoView()
+              }
+            >
+              Projects
+            </button>
+            <button
+              className="h-10 w-28 transition-colors duration-300 delay-100 hover:bg-indigo-500"
+              onClick={() =>
+                document.getElementById("contact").scrollIntoView()
+              }
+            >
+              Contact
+            </button>
+          </div>
+        </div>
       </div>
 
       <div className="hidden h-12 sm:flex justify-center space-x-3 mx-auto font-semibold">
