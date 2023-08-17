887bed732968703e47815da07a1818bbff02d8a4
Augustine Hauge <augustinehauge@gmail.com>
2023-08-17 16:16:19 -0400

    Functioing dropdown

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,66 @@
 import { Bars3Icon } from "@heroicons/react/24/solid";
+import { useState } from "react";
 
 export const Navbar = () => {
+  const [expanded, setExpanded] = useState(false);
+
   return (
-    <div class="flex justify-between bg-indigo-900 items-center">
+    <div class="flex justify-between bg-indigo-900 items-center h-12 w-full">
       <div class="shrink-0 mx-6">
         <img src="/images/Gus_logo.png" alt="GitHub" class="w-8 rounded-full" />
       </div>
 
-      <div class="flex w-full h-12 justify-center space-x-3 mx-auto font-semibold">
+      <div class="flex flex-col items-center relative sm:hidden">
+        <button onClick={() => setExpanded(!expanded)}>
+          <Bars3Icon class="h-10" />
+        </button>
+        {expanded && (
+          <>
+            <div class="flex flex-col absolute mt-10 items-center justify-center my-auto bg-indigo-900">
+              <button
+                class="h-10 w-28 transition-colors duration-300 delay-100 hover:bg-indigo-500"
+                onClick={() => window.scrollTo(0, 0)}
+              >
+                Home
+              </button>
+              <button
+                class="h-10 w-28 transition-colors duration-300 delay-100 hover:bg-indigo-500"
+                onClick={() =>
+                  document.getElementById("about").scrollIntoView()
+                }
+              >
+                About
+              </button>
+              <button
+                class="h-10 w-28 transition-colors duration-300 delay-100 hover:bg-indigo-500"
+                onClick={() =>
+                  document.getElementById("experience").scrollIntoView()
+                }
+              >
+                Experience
+              </button>
+              <button
+                class="h-10 w-28 transition-colors duration-300 delay-100 hover:bg-indigo-500"
+                onClick={() =>
+                  document.getElementById("projects").scrollIntoView()
+                }
+              >
+                Projects
+              </button>
+              <button
+                class="h-10 w-28 transition-colors duration-300 delay-100 hover:bg-indigo-500"
+                onClick={() =>
+                  document.getElementById("contact").scrollIntoView()
+                }
+              >
+                Contact
+              </button>
+            </div>
+          </>
+        )}
+      </div>
+
+      <div class="hidden sm:flex justify-center space-x-3 mx-auto font-semibold">
         <button
           class="px-4 rounded-md transition-colors duration-300 delay-100 hover:bg-indigo-500"
           onClick={() => window.scrollTo(0, 0)}
