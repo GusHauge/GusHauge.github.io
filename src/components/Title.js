d99a8bd08a1e39f006a3ec45722f473de6e0b952
Augustine Hauge <augustinehauge@gmail.com>
2023-08-17 09:03:47 -0400

    Added Contact info

--- src/components/Title.js
+++ src/components/Title.js
@@ -3,7 +3,7 @@ export const Title = () => {
     <div class="flex flex-col w-full h-screen bg-gradient-to-t from-black to-indigo-900 space-y-10 items-center text-center">
       <h1 class="text-7xl mt-[25vh] font-thin">Gus Hauge</h1>
       <h2 class="text-xl font-light">
-        Software Engineer with a passion for people and technology
+        Software Engineer leveraging technology for a better world
       </h2>
     </div>
   );
