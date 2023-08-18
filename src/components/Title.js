ca339538e8ececa3605fd9100874a155525f2486
Augustine Hauge <augustinehauge@gmail.com>
2023-08-17 21:48:19 -0400

    Title resizing

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,8 +1,8 @@
 export const Title = () => {
   return (
     <div className="flex flex-col w-full h-screen bg-gradient-to-t from-black to-indigo-900 space-y-10 items-center text-center px-10">
-      <h1 className="text-7xl mt-[25vh] font-thin">Gus Hauge</h1>
-      <h2 className="text-xl font-light">
+      <h1 className="text-5xl sm:text-7xl mt-[25vh] font-thin">Gus Hauge</h1>
+      <h2 className="text-base sm:text-xl font-light">
         Curious software engineer with a passion for people and technology
       </h2>
     </div>
