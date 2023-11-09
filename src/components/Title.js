349705d1d917f1e3fc2076a374d3c4b543249be4
Augustine Hauge <augustinehauge@gmail.com>
2023-11-08 20:20:00 -0500

    Changed headline

--- src/components/Title.js
+++ src/components/Title.js
@@ -3,7 +3,7 @@ export const Title = () => {
     <div className="flex flex-col w-full h-screen bg-gradient-to-t from-black to-indigo-900 space-y-10 items-center text-center px-10">
       <h1 className="text-6xl sm:text-7xl mt-[25vh] font-thin">Gus Hauge</h1>
       <h2 className="text-base sm:text-xl font-light">
-        Curious software engineer with a passion for people and technology
+        Software engineer with a passion for people and technology
       </h2>
     </div>
   );
