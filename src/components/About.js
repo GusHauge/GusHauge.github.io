cb9e96a0d01e702ba86d025e3939d178c7f84dd0
Augustine Hauge <augustinehauge@gmail.com>
2025-05-23 15:18:32 -0500

    Misc. updates

--- src/components/About.js
+++ src/components/About.js
@@ -8,21 +8,22 @@ export const About = () => {
       <div className="flex flex-col sm:flex-row items-center justify-center space-y-6 sm:space-x-8 sm:space-y-0 md:space-x-12 lg:space-x-16 xl:space-x-20">
         <div className="shrink-0 sm:w-1/3 lg:w-1/4 xl:w-1/3">
           <img
-            src="/images/Bray_Frontal.jpg"
+            src="/images/Headshot.jpg"
             alt="Gus"
             className="rounded-lg object-cover w-96 h-64 sm:w-full sm:h-auto"
           />
         </div>
         <p className="text-base md:text-lg lg:text-xl max-w-lg">
-          Hi there! I am a full stack software developer at IBM and a recent
-          graduate of the University of Notre Dame currently living in Austin,
-          Texas. Throughout my young career, I have been able to sharpen my
-          skills in both front-end and back-end development with full stack
-          projects in my personal and professional work. At the moment, I am
-          most experienced in web development but I also have a great interest
-          in data visualization, ML-driven analytics, and computer vision. I am
-          always on the hunt for new applications of software and I hope for a
-          world where tech is used to help people in the areas of greatest need.
+          Hi, I’m Gus - a software developer who enjoys building tools that make
+          life simpler, faster, and more enjoyable. I’ve had the chance to work
+          on everything from full-stack web apps to infrastructure automation at
+          IBM, where I’ve learned the value of clean code, good design, and
+          reliable systems. I studied Computer Science and Theology at Notre
+          Dame, which means I care deeply about both how things work and why
+          they matter. Whether I’m designing a new feature, refining user
+          experience, or diving into DevOps workflows, I’m always looking for
+          ways to grow, collaborate, and create something meaningful. Thanks for
+          stopping by - feel free to check out my projects and reach out!
         </p>
       </div>
     </div>
